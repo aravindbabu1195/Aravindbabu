7acf85ffd5b30b44a88e4400611e2c4bcd705c4a
Aravind Babu <aravindbabu1195@gmail.com>
2023-10-01 15:16:40 +0530

    upload photo

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text',{
-strings: ['Frontend Developer','YouTuber','Blogger'],
+strings: ['Python Developer','Frontend Developer','YouTuber'],
 typeSpeed: 100,
 backSpeed: 100,
 backDelay: 1000,
